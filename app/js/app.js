341fc5ab77ff959f0df46b1e2cf90fb3a9ad7991
Pedro Souza <predoff@gmail.com>
2017-11-20 20:36:45 -0200

    Adicionando express para executar aplicativo localmente e editando README com instrucoes da solucao

--- app/js/app.js
+++ app/js/app.js
@@ -44,56 +44,56 @@
   var restaurantsList = [
   {
     "name": "Fogão Mineiro",
-    "url": "images/resources/img2.jpg",
+    "url": "images/img2.jpg",
 
     "likes": 0,
     "dislikes": 10
   },
   {
     "name": "Burger King",
-    "url": "images/resources/img4.jpg",
+    "url": "images/img4.jpg",
 
     "likes": 0,
     "dislikes": 0
   },
   {
     "name": "Five Guys",
-    "url": "images/resources/img5.jpg",
+    "url": "images/img5.jpg",
 
     "likes": 0,
     "dislikes": 0
   },
   {
     "name": "Kaizen Sushi",
-    "url": "images/resources/img6.jpg",
+    "url": "images/img6.jpg",
 
     "likes": 0,
     "dislikes": 0
   },
   {
     "name": "Aô Sushi Bar",
-    "url": "images/resources/img7.jpg",
+    "url": "images/img7.jpg",
 
     "likes": 0,
     "dislikes": 0
   },
   {
     "name": "Burguer House",
-    "url": "images/resources/img8.jpg",
+    "url": "images/img8.jpg",
 
     "likes": 0,
     "dislikes": 0
   },
   {
     "name": "Steak House ",
-    "url": "images/resources/img9.jpg",
+    "url": "images/img9.jpg",
 
     "likes": 0,
     "dislikes": 0
   },
   {
     "name": "Coco Bambu",
-    "url": "images/resources/img10.jpg",
+    "url": "images/img10.jpg",
 
     "likes": 0,
     "dislikes": 3
